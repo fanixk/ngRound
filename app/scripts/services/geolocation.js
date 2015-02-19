74cae0e68ee4386a94435927f55389d6af863d03
fanixk <katsimpas.f@gmail.com>
2015-02-19 03:05:45 +0200

    dry

--- app/scripts/services/geolocation.js
+++ app/scripts/services/geolocation.js
@@ -9,28 +9,28 @@
  */
 angular.module('ngRoundApp')
   .factory('geoLocation', function ($q, $rootScope, geolocationErrorMsgs) {
+    var deferred = $q.defer();
+
     return {
 
       getPosition: function() {
-        var deferred = $q.defer();
 
         if(navigator.geolocation) {
+
           navigator.geolocation.getCurrentPosition(function(position) {
             $rootScope.$apply(function() {
               deferred.resolve(position);
             });
-          }, function(error) { this._error(error, deferred); });
+          }, function(error) { this._error(error); });
+
         } else {
           var error = geolocationErrorMsgs.errors.locacation.unsupportedBrowser;
-          $rootScope.$broadcast('error', error);
-          $rootScope.$apply(function() {
-            deferred.reject(error);
-          });
+          this._rejectAndBroadcast(error);
         }
 
         return deferred.promise;
       },
-      _error: function(error, deferred) {
+      _error: function(error) {
         var errMsg;
         switch (error.code) {
           case 1:
@@ -43,6 +43,9 @@ angular.module('ngRoundApp')
             errMsg = geolocationErrorMsgs.errors.location.timeout;
             break;
         }
+
+      },
+      _rejectAndBroadcast: function(errMsg) {
         $rootScope.$broadcast('error', errMsg);
         $rootScope.$apply(function() {
           deferred.reject(errMsg);
